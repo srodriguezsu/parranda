e6a1fd3a8bb93703ed29b4f818233f0cdf857da8
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-06-05 21:41:50 -0500

    Homepage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './homePage.css'; // Assuming you have a CSS file for styling
 
 const HomePage = () => {
     return (
         <div>
-
+            <h1 className="main-titulo">Parranda Navideña</h1>
+            <img
+                src="/public/pesebre.svg"
+                alt="Christmas Tree"
+                className="absolute left-10 top-20 w-40 h-auto"
+            />
         </div>
     );
 };
--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            
+            <h1>No pudimos encontrar lo que buscabas</h1>
         </div>
     );
 };
