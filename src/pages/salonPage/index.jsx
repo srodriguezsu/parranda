665d2aeebfb28832503649148e2bc8b79e4437a9
juanp <jperezalv@unal.edu.co>
2025-06-23 23:44:53 -0500

    Added other song

--- src/pages/salonPage/index.jsx
+++ src/pages/salonPage/index.jsx
@@ -1,50 +1,42 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from "../../components/header/index.jsx";
 import Cancion from './cancion.jsx';
 import Reproductor from './reproductor';
 import ModalCanciones from './modalCanciones.jsx';
 import './index.css';
 
-
-// Listas de canciones, tanto las que iran a la izquierda como a la derecha
+// Listas de canciones, tanto las que irán a la izquierda como a la derecha
 const listaCancionesIzq = [
   { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
   { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
+  { nombre: "Aguila Roja", artista: "Comercial 3D Navidad", audioSrc: "src/assets/canciones/Aguila Roja comercial 3d Navidad.mp3" },
   { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
   { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
+  { nombre: "Aguila Roja", artista: "Comercial 3D Navidad", audioSrc: "src/assets/canciones/Aguila Roja comercial 3d Navidad.mp3" },
   { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
   { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
+  { nombre: "Aguila Roja", artista: "Comercial 3D Navidad", audioSrc: "src/assets/canciones/Aguila Roja comercial 3d Navidad.mp3" },
   { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
-  { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
-  { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
-  { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
-  { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
-  { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
-  { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" }
 ];
 
 const listaCancionesDer = [
   { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
   { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
+  { nombre: "Aguila Roja", artista: "Comercial 3D Navidad", audioSrc: "src/assets/canciones/Aguila Roja comercial 3d Navidad.mp3" },
   { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
   { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
+  { nombre: "Aguila Roja", artista: "Comercial 3D Navidad", audioSrc: "src/assets/canciones/Aguila Roja comercial 3d Navidad.mp3" },
   { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
   { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
-  { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
-  { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
-  { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
-  { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
-  { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },
-  { nombre: "El Ausente", artista: "Pastor López", audioSrc: "src/assets/canciones/EL AUSENTE - PASTOR LOPEZ.mp3" },
-  { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" }
+  { nombre: "Aguila Roja", artista: "Comercial 3D Navidad", audioSrc: "src/assets/canciones/Aguila Roja comercial 3d Navidad.mp3" },
+  { nombre: "Cariñito", artista: "Rodolfo Aicardi", audioSrc: "src/assets/canciones/CARIÑITO - RODOLFO AICARDI.mp3" },  
 ];
 
-// Lista de canciones combinada para que el reproductor pueas acceder a todas
+// Lista de canciones combinada para que el reproductor pueda acceder a todas
 const listaCanciones = [...listaCancionesIzq, ...listaCancionesDer];
 
 // Componente principal de la página del salón de parrandas
 const ParrandasPage = () => {
-
   // Estado para manejar si la pantalla es móvil o no
   const [esMovil, setEsMovil] = useState(window.innerWidth < 1060);
   const [mostrarModal, setMostrarModal] = useState(false);
@@ -56,21 +48,18 @@ const ParrandasPage = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-    // Estado para manejar el índice de la canción actual
+  // Estado para manejar el índice de la canción actual
   const [indiceActual, setIndiceActual] = useState(0);
   const cancionActual = listaCanciones[indiceActual];
 
   return (
-
-
     <div>
       <Header pageTitle={"Salón de Parrandas"} />
 
       <main>
-        {/*Si es móvil, mostramos un botón para ver las canciones y el reproductor */}
+        {/* Si es móvil, mostramos un botón para ver las canciones y el reproductor */}
         {esMovil ? (
           <>
-
             <button className="btnMostrarCanciones" onClick={() => setMostrarModal(true)}>
               Ver Canciones
             </button>
@@ -97,8 +86,7 @@ const ParrandasPage = () => {
         ) : (
           <>
             {/* Si no es móvil, mostramos las canciones a la izquierda y derecha del reproductor */}
-            {/* Se crea un componente de canción por cada canción en las listas de la izquierda y derecha. */}
-            <div className='cancionesIzq'>
+            <div className="cancionesIzq">
               {listaCancionesIzq.map((c, i) => (
                 <Cancion
                   key={`izq-${i}`}
@@ -110,14 +98,14 @@ const ParrandasPage = () => {
               ))}
             </div>
 
-            {/* Se crea un componente de tipo reproductor que muestra la canción actual y permite navegar entre las canciones */}
+            {/* Componente reproductor que muestra la canción actual y permite navegar entre canciones */}
             <Reproductor
               cancion={cancionActual}
               siguiente={() => setIndiceActual((prev) => (prev + 1) % listaCanciones.length)}
               anterior={() => setIndiceActual((prev) => (prev - 1 + listaCanciones.length) % listaCanciones.length)}
             />
 
-            <div className='cancionesDer'>
+            <div className="cancionesDer">
               {listaCancionesDer.map((c, i) => (
                 <Cancion
                   key={`der-${i}`}
@@ -131,7 +119,6 @@ const ParrandasPage = () => {
           </>
         )}
       </main>
-
     </div>
   );
 };
