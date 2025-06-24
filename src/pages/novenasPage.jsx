b9a26ed26616f1292018e8ed8104455ef4a69f97
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-06-23 21:39:39 -0500

    Improved animations

--- src/pages/novenasPage.jsx
+++ src/pages/novenasPage.jsx
@@ -20,39 +20,36 @@ const NovenasPage = () => {
                 </div>
 
                 <div className="contenido-central">
-                    <div className="contenido-central">
 
-                        <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
-                        <BotonNovena texto="Oración para todos los días" ancho="90%" />
-                        </div>
-
-                        <div style={{ display: "flex", justifyContent: "center", gap: "1rem", flexWrap: "wrap" }}>
-                            <BotonNovena texto="Avemaría" />
-                            <BotonNovena texto="Padrenuestro" />
-                            <BotonNovena texto="Gloria" />
-                        </div>
-
-                        <div style={{ display: "flex", justifyContent: "center", gap: "1rem", flexWrap: "wrap" }}>
-                        {[...Array(9)].map((_, i) => (
-                        <BotonNovena key={i} texto={`Día ${i + 1}`} />
-                        ))}
-                        </div>
-
-                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                        <BotonNovena texto="Oración a la Santísima Virgen" ancho="90%" />
-                        <BotonNovena texto="Oración a San José" ancho="90%" />
-                        <BotonNovena texto="Oración al Niño Jesús" ancho="90%" />
-                        <BotonNovena texto="Aspiraciones para la venida del Niño Dios" ancho="90%" />
-                        </div>
+                    <div style={{display: "flex", justifyContent: "center", flexWrap: "wrap"}}>
+                        <BotonNovena texto="Oración para todos los días" ancho="90%"/>
                     </div>
 
+                    <div style={{display: "flex", justifyContent: "center", gap: "1rem", flexWrap: "wrap"}}>
+                        <BotonNovena texto="Avemaría"/>
+                        <BotonNovena texto="Padrenuestro"/>
+                        <BotonNovena texto="Gloria"/>
+                    </div>
+
+                    <div style={{display: "flex", justifyContent: "center", gap: "1rem", flexWrap: "wrap"}}>
+                        {[...Array(9)].map((_, i) => (
+                            <BotonNovena key={i} texto={`Día ${i + 1}`}/>
+                        ))}
+                    </div>
+
+                    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                        <BotonNovena texto="Oración a la Santísima Virgen" ancho="90%"/>
+                        <BotonNovena texto="Oración a San José" ancho="90%"/>
+                        <BotonNovena texto="Oración al Niño Jesús" ancho="90%"/>
+                        <BotonNovena texto="Aspiraciones para la venida del Niño Dios" ancho="90%"/>
+                    </div>
                 </div>
 
                 <div className="columna-derecha">
-                    <ReproductorVillancicos />
+                    <ReproductorVillancicos/>
                     <div className="velas-base">
-                        <Vela className="vela-izquierda" />
-                        <Vela className="vela-derecha" />
+                        <Vela className="vela-izquierda"/>
+                        <Vela className="vela-derecha"/>
                     </div>
                 </div>
             </div>
